72a5336b6b6b2b46f2a8b7bc18c5e76f8d5aea07
Hisham Radi <code.spacing@gmail.com>
2019-03-30 17:18:57 +0100

    Add the "value" & "unit" to the hidden input
    Add and/or Update the hidden input with the "value" + "unit"

--- js/unit.js
+++ js/unit.js
@@ -1,3 +1,23 @@
 (function( $ ){
+	
+	var unit_all = function(field_id){
 
+		if(typeof field_id === 'undefined')
+			return;	
+			
+		var field_value = $('input.cmb2-unit-input[data-field-id="' + field_id + '"]').val();
+		var field_unit = $('select.cmb2-unit-select[data-field-id="' + field_id + '"]').val();	
+		
+		$('input[type=hidden][data-field-id="' + field_id + '"]').val(field_value + field_unit);	
+			
+	};
+	
+	$('select.cmb2-unit-select').on('change', function(){
+		unit_all($(this).attr('data-field-id'));
+	});
+	
+	$('input.cmb2-unit-input').on('input propertychange paste', function(){
+		unit_all($(this).attr('data-field-id'));		
+	});
+	
 })( jQuery );
